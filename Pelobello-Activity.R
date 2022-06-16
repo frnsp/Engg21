8377b79ba881897671c3bf68c83fcb41fb321d46
frnsp <fernan.pelobello@obf.ateneo.edu>
2022-06-16 15:55:28 +0800

    Revision

--- Pelobello-Activity.R
+++ Pelobello-Activity.R
@@ -1,3 +1,3 @@
-print("Hello po! My name Fernan Frans B. Pelobello.")
+print("Hello po! My name is Fernan Frans B. Pelobello.")
 print("My nickname is Fernan.")
 print("My favorite hobby is watching series in Netflix.")
